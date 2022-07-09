a1b7c65b5fc8638309a53a4bab468dfd15cb183f
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-07-09 19:19:50 +0300

    Loader

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import SearchFrom from 'components/SearchForm';
 
-import styles from '../Searchbar/Searchbar.module.css'
+
+import styles from './Searchbar.module.css';
+
 
 const Searchbar = ({ onSearch }) => (
     <header className={styles.Searchbar}>
